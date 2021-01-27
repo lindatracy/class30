db589ac185e5ea2794f805cff0699857fb22f094
lindatracy <lindatracy3105@gmail.com>
2021-01-27 16:57:16 +0530

    vanishing pigs

--- Slingshot.js
+++ Slingshot.js
@@ -3,7 +3,7 @@ class SlingShot{
         var options = {
             bodyA: bodyA,
             pointB: pointB,
-            stiffness: 0.04,
+            stiffness: 0.1,
             length: 10
         }
         this.sling1 = loadImage('sprites/sling1.png');
@@ -12,6 +12,10 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+    }
+
+    attach(body){
+        this.sling.bodyA=body;
     }
 
     fly(){
